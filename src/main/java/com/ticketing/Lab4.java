987e40713525158866c8e8fc28720574f0ca92d1
rhildred <rhildred@gmail.com>
2015-08-04 09:20:30 -0400

    oops committed broken code

---
+++ src/main/java/com/ticketing/Lab4.java
@@ -0,0 +1,31 @@
+package com.ticketing;
+
+import ca.on.conestogac.*;
+
+import java.sql.*;
+
+public class Lab4 {
+	public static void main(String[] args) {
+		//set these to be null so that we can finally close them
+        Connection connection = null;
+        Statement oStmt = null;
+        try{
+        	//make a stmt from my SQL
+        	connection = OpenShiftDataSource.getConnection("jdbc:mysql://localhost:3306/kanbanpomodoro?user=root");
+        	oStmt = connection.createStatement();
+        	String sSQL = "SELECT * FROM PERSON";
+        	ResultSet oRs = oStmt.executeQuery(sSQL);
+        	System.out.println(ResultSetValue.toJsonString(oRs));
+            oRs.close();
+        }catch(Exception e){
+        	e.printStackTrace();
+        }finally{
+        	try{
+        		if(oStmt != null) oStmt.close();
+        		if(connection != null) connection.close();
+        	}catch(Exception e){
+        		e.printStackTrace();
+        	}
+        }
+	}
+}
